a3c322123bfa809617ab627c7f3d739bcd2ded97
Zachary Vanscoit <zachary816@gmail.com>
2020-04-19 07:43:03 -0500

    first attempt at differential coding

---
+++ js/helper.js
@@ -0,0 +1,66 @@
+/*
+These are helper functions that can be used throughout the code.
+*/
+let startDate = new Date(2020, 0, 22);
+
+// Returns a string with hours and minutes
+function msToTime(duration) {
+    var milliseconds = parseInt((duration % 1000) / 100),
+      seconds = Math.floor((duration / 1000) % 60),
+      minutes = Math.floor((duration / (1000 * 60)) % 60),
+      hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+  
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+  
+    return hours + ":" + minutes;
+}
+
+// Will generate an array of dates to filter through for the time slider
+function getDateArray() {
+    var temp = new Array();
+    var length = date_diff_indays(new Date(2020, 1, 22), getToday());
+
+    // create initial date array
+    for (var i = 0; i < length; i++) {
+        temp.push(addDays(startDate, i))
+    }
+    return temp;
+}
+
+// Days between 2 dates
+var date_diff_indays = function(date1, date2) {
+    dt1 = new Date(date1);
+    dt2 = new Date(date2);
+    return Math.abs(Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24)));
+}
+
+// Used to get todays date
+function getToday() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth()).padStart(2, '0'); // 0 starting index
+    var yyyy = today.getFullYear();
+
+    return new Date(yyyy, mm, dd);
+}
+
+// give a nice date format to print
+function niceDate(date) {
+    var dd = String(date.getDate()).padStart(2, '0');
+    var mm = String(date.getMonth() + 1).padStart(2, '0'); // 0 starting index
+    var yyyy = date.getFullYear();
+
+    return mm + "/" + dd + "/" + yyyy;
+}
+
+// This function will transfer days from to a readable date
+function addToDate(date, days) {
+    Date.prototype.addDays = function(days) {
+        var date = new Date(this.valueOf());
+        date.setDate(date.getDate() + days);
+        return date;
+    }
+    return date.addDays(days);
+}
--- js/map_init.js
+++ js/map_init.js
@@ -1,41 +1,107 @@
 // Global objects
 let map = null;         // Mapbox GL object
-let geojson = null;     // geojson object
+let geojson = null;     // Geojson object
 let positions = null;   // 2D array of longitude and latitude
+let counties = null;    // Counties object
+let erics = null;
+let dateBins = null;
 
 // initializer functions -------------------------------------------------------
 // main initializer
 function mainInit() {
+    // load erics
+    $.ajax({
+        method: "GET",
+        url: "https://eric.clst.org/assets/wiki/uploads/Stuff/gz_2010_us_050_00_20m.json",
+        processData: false,
+        contentType: false,
+        encType: "multipart/form-data",
+        success: loadEricData,
+        error: ajaxErrorHandle
+    });
+
+    // load ours
     $.ajax({
         method: "GET",
         url: config.server_ip + "/countyLocationData",  // config.server_remote, config.server_local
         processData: false,
         contentType: false,
         encType: "multipart/form-data",
-        success: loadInitialData
+        success: loadInitialData,
+        error: ajaxErrorHandle
     });
 
-    initMap([3, 11]);
-    
-    displayFooterMessage("This is the text that will be displayed", true);
+    displayFooterMessage("Loading initial county data, please wait...", false);
+
     $("#uploadForm").submit(loadJSON)
     $("input[name='data-consent']").change(checkBoxStatusChange)
 }
 
+// load erics while moving stuff around in the background
+function loadEricData(data){
+    erics = data;
+    displayFooterMessage("Erics data loaded.", false);
+    initMap(erics, [4, 11]);
+}
 
+// initial ajax call of data load
 function loadInitialData(data){
-    positions = JSON.parse(data);
-    alert("data loaded");
+    counties = JSON.parse(data);
+    console.log(niceDate(getToday()));
+
+    // hopefully erics data doesn't change
+    for (var i = 0; i < counties.length; i++) {
+        // cut out the first part of the geoid
+        var geoid = erics.features[i].properties.GEO_ID.substring(9,);
+        // our cases array that will change as we reconstruct the differential encoding
+        var cases = counties[i].confirmed_cases;
+        // make sure there are cases in the county
+        if (cases.length > 0) {
+            // current index
+            var casesIndex = 0;
+            // most recently changed date in the for current index
+            var lastChangedDate = addToDate(startDate, cases[0].daysElapsed);
+            // the number of days from our first case until now
+            var daysFromFirst = date_diff_indays(lastChangedDate, getToday());
+            // iterate from the first day in cases until today
+            for (var j = 0; j < daysFromFirst; j++) {
+                // if we're on the last array of the casses, we just need to copy it until todays date to display
+                if ((casesIndex + 1) >= cases.length) {
+                    cases.push({'daysElapsed':cases[casesIndex].daysElapsed,'date':addToDate(startDate, cases[casesIndex].daysElapsed + j),'count':cases[casesIndex].count});
+                    casesIndex ++;
+                } else {
+                    cases[casesIndex]['date'] = addToDate(startDate, cases[casesIndex].daysElapsed);
+                    if (cases[casesIndex].daysElapsed != cases[casesIndex + 1].daysElapsed) {
+                        cases.splice(casesIndex + 1, 0, {'daysElapsed':cases[casesIndex].daysElapsed + j,'date':addToDate(startDate, cases[casesIndex].daysElapsed + j),'count':cases[casesIndex].count});
+                    } else {
+                        lastChangedDate = addToDate(startDate, cases[casesIndex].daysElapsed + j);
+                        casesIndex ++;
+                    }
+                }
+            }
+        }
+        // just a double check if theres an inconsitency in data, we really just wanna break out of the loop
+        if (geoid == counties[i].GEO_ID) {
+            erics.features[i].properties['confirmed_cases'] = counties[i].confirmed_cases;
+            erics.features[i].properties['deaths'] = counties[i].deaths;
+        } else {
+            displayFooterMessage("An error occured with combining erics data.", true);
+            console.log(geoid);
+            break;
+        }
+    }
+    displayFooterMessage("County data loaded.", false);
+    console.log(erics);
 }
 
 // initializes map
-function initMap(zoom) {
+function initMap(data, zoom) {
     mapboxgl.accessToken = 'pk.eyJ1IjoiemFjaGFyeTgxNiIsImEiOiJjazd6NXN2eWwwMml0M2tvNGo2c3JkcGFpIn0.aB1upejZ61JQjb_z2g1NuA';
     map = new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
+        style: 'mapbox://styles/mapbox/light-v10',
         center: [-89.651607, 39.781232],
-        zoom: 9,
+        zoom: 7,
         minZoom: zoom[0],
         maxZoom: zoom[1]
     });
@@ -44,9 +110,9 @@ function initMap(zoom) {
 
     // init of map with blank geojson
     map.on('load', function () {
-        map.addSource('point', {
+        map.addSource('county', {
             'type': 'geojson',
-            'data': 'https://eric.clst.org/assets/wiki/uploads/Stuff/gz_2010_us_050_00_20m.json'
+            'data': data
             //'data': geojson
         });
         // Heatmap
@@ -100,17 +166,6 @@ function initMap(zoom) {
         //       },
         //     }
         // }, 'waterway-label');
-
-        map.addLayer({
-            'id': 'county-layer',
-            'type': 'fill',
-            'source': 'point',
-            'paint': {
-                'fill-color': 'rgba(200, 100, 240, 0.4)',
-                'fill-outline-color': 'rgba(200, 100, 240, 1)'
-            }
-        });
-
         // Adding circles
         // map.addLayer({
         //     id: 'user-position',
@@ -145,42 +200,82 @@ function initMap(zoom) {
         //         'circle-stroke-color': 'white',
         //         'circle-stroke-width': 1,
         //         'circle-opacity': {
-        //         stops: [
-        //             [14, 0],
-        //             [15, 1]
-        //         ]
+        //              stops: [
+        //                  [14, 0],
+        //                  [15, 1]
+        //              ]
         //         }
         //     }
-        //     }, 'waterway-label');
+        // }, 'waterway-label');
         // Create a popup, but don't add it to the map yet.
+
+        map.addLayer({
+            'id': 'county-layer',
+            'type': 'fill',
+            'source': 'county',
+            'minzoom': 5.5,
+            'paint': {
+                'fill-color': 'rgba(150, 75, 150, 0.4)',
+                'fill-outline-color': 'rgba(50, 0, 50, 0.8)'
+            }
+        });
+
+        map.addLayer({
+            'id': 'county-labels',
+            'type': 'symbol',
+            'source': 'county',
+            'minzoom': 8,
+            'layout': {
+                'text-field': [
+                    'concat',
+                    ['to-string', ['get', 'NAME']],
+                    '\nCases:'
+                ],
+                'text-font': [
+                    'Open Sans Bold',
+                    'Arial Unicode MS Bold'
+                ],
+                'text-size': 14
+            },
+                'paint': {
+                    'text-color': 'rgba(0,0,0,1)'
+                }
+        });
+
         var popup = new mapboxgl.Popup({
             closeButton: false,
             closeOnClick: false
-            });
+        });
             
-            map.on('mouseenter', 'user-position', function(e) {
-                // Change the cursor style as a UI indicator.
-                map.getCanvas().style.cursor = 'pointer';
-                
-                var coordinates = e.features[0].geometry.coordinates;
-                var description = e.features[0].properties.description;
-                
-                // Ensure that if the map is zoomed out such that multiple
-                // copies of the feature are visible, the popup appears
-                // over the copy being pointed to.
-                while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-                    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-                }
-                // Populate the popup and set its coordinates
-                // based on the feature found.
-                popup.setLngLat(coordinates).setHTML(description).addTo(map);
-            });
+        map.on('mouseenter', 'user-position', function(e) {
+            // Change the cursor style as a UI indicator.
+            map.getCanvas().style.cursor = 'pointer';
             
-            map.on('mouseleave', 'user-position', function() {
+            var coordinates = e.features[0].geometry.coordinates;
+            var description = e.features[0].properties.description;
+            
+            // Ensure that if the map is zoomed out such that multiple
+            // copies of the feature are visible, the popup appears
+            // over the copy being pointed to.
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
+            // Populate the popup and set its coordinates
+            // based on the feature found.
+            popup.setLngLat(coordinates).setHTML(description).addTo(map);
+        });
+            
+        map.on('mouseleave', 'user-position', function() {
             map.getCanvas().style.cursor = '';
             popup.remove();
         });
     });
+}
+
+// filter by function for map
+function filterBy(date) {
+    var dateArray = getDateArray();
+    var filters = ['==', 'date', date];
 }
 
 // initializes geojson
@@ -261,20 +356,6 @@ function populatePoints(json_data) {
     }
 }
 
-// Returns a string with hours and minutes
-function msToTime(duration) {
-    var milliseconds = parseInt((duration % 1000) / 100),
-      seconds = Math.floor((duration / 1000) % 60),
-      minutes = Math.floor((duration / (1000 * 60)) % 60),
-      hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-  
-    hours = (hours < 10) ? "0" + hours : hours;
-    minutes = (minutes < 10) ? "0" + minutes : minutes;
-    seconds = (seconds < 10) ? "0" + seconds : seconds;
-  
-    return hours + ":" + minutes;
-}
-
 // load JSON function called from button press
 function loadJSON(e) {
     formdata = new FormData();
@@ -315,7 +396,8 @@ function loadJSON(e) {
                 populatePoints(json_data);
             }
             $("#loginModal")[0].style.display="none";
-        }
+        },
+        error: ajaxErrorHandle
     });
     e.preventDefault();
 }
