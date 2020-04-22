07abb44eb17bf739c2f7a1c4a435049f0dae245e
Sai Shanmukha Narumanchi <nss10@outlook.com>
2020-04-22 02:19:53 -0500

    Added transparency to the color

--- python/helper.py
+++ python/helper.py
@@ -1,46 +1,44 @@
-import json,time,requests,numpy as np,pandas as pd
-from shapely.geometry import Point,Polygon
+import json
+import time
+import requests
+import numpy as np
+import pandas as pd
+from shapely.geometry import Point, Polygon
 
-def getTimeOverlap(infectedTime,userTime):
+
+def getTimeOverlap(infectedTime, userTime):
     '''
     Computes the difference in time in terms of number of hours and returns a floating point number
     '''
     if(userTime["endTimestampMs"] < infectedTime["startTimestampMs"]):
         #  infected person visited after user left : No Chance of overlap
-        return -1 
+        return -1
 
     elif(infectedTime["endTimestampMs"] < userTime["startTimestampMs"]):
         # user visited after infected person left : Returns the time gap between visits
-        diff = int(userTime["startTimestampMs"]) - int(infectedTime["startTimestampMs"])
+        diff = int(userTime["startTimestampMs"]) - \
+            int(infectedTime["startTimestampMs"])
         return diff
 
     else:
-        # both are at the same place with some time overlap 
+        # both are at the same place with some time overlap
         return 0
 
 
 def getTimeSince(infectedTime):
-    diff = time.time()- int(infectedTime["startTimestampMs"])
+    diff = time.time() - int(infectedTime["startTimestampMs"])
     return diff
+
 
 def get_json_from_path(path):
     with open(path) as json_file:
-            return json.load(json_file)          
+        return json.load(json_file)
 
 
 def get_quantile(arr):
-    low=200
-    up=-1
-    labelList = ["#FFCC99", "#FFCC33", "#FFCC00","#FF9933","#FF9900","#FF6666","#FF6633","#FF6600","#FF0033","#FF0000","#CC0033" ]
-    x = pd.qcut(arr,len(labelList),labels=labelList)
-    return list(zip(arr,list(x)))
-
-
-#Work in Progress
-def getCountyFromPoint():
-    p1 = Point(24.952242, 60.1696017)
-    content = requests.get("https://geo.fcc.gov/api/census/area?lat="+str(24.952242)+"&lon="+str(60.1696017)+"&format=json")
-    jsonObj = json.loads(content.content)
-    print(jsonObj)
-
-# isPointInCounty()
+    low = 200
+    up = -1
+    labelList = ["rgba(255, 204, 153, 0.4)", "rgba(255, 204, 51, 0.4)", "rgba(255, 204, 0, 0.4)", "rgba(255, 153, 51, 0.4)", "rgba(255, 153, 0, 0.4)",
+                 "rgba(255, 102, 102, 0.4)", "rgba(255, 102, 51, 0.4)", "rgba(255, 102, 0, 0.4)", "rgba(255, 0, 51, 0.4)", "rgba(255, 0, 0, 0.4)", "rgba(204, 0, 51, 0.4)"]
+    x = pd.qcut(arr, len(labelList), labels=labelList)
+    return list(zip(arr, list(x)))
