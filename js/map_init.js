69eed079a25d81914b1dc53a24107016e36bd859
Sai Shanmukha Narumanchi <nss10@outlook.com>
2021-02-02 16:59:01 -0600

    Restructured Ajax calls with Async/await and promises

--- js/map_init.js
+++ js/map_init.js
@@ -1,35 +1,35 @@
 // Global objects
 let map = null;         // Mapbox GL object
-let geojson = null;     // Geojson object
-let positions = null;   // 2D array of longitude and latitude
-let counties = null;    // Counties object from our backend
-let geoJson = null;       // Erics county geometry data
-let allData = null;     // All data in the whole universe
-let allDataGeo = null;  // All data but in mapbox readable format
 let dateList = null;    // List of dates to filter by
-var geoJsonDataAttempts = 0;   // Attempts to grab erics data
-var maxPropValForColorCode = 0;           // Max cases to determine the min and max for gradient
-var currentDate = '';       // Current day determined from the slider
-var currentDayValue = 0;    // Numerical value of current day determined from the slider
-var latestDateValue;
-var lastDayElapsed;
-var mapStyle = 'mapbox://styles/mapbox/dark-v10';
-var uploadOption = "countyLevel";    // countyLevel, infectedPlaces, etc..
+let maxPropValForColorCode = 0; // Max cases to determine the min and max for gradient
+let currentDate = '';       // Current day determined from the slider
+let currentDayValue = 0;    // Numerical value of current day determined from the slider
+let latestDateValue;
+let lastDayElapsed;
+const mapStyle = 'mapbox://styles/mapbox/dark-v10';
 const countKey = config['ccd']['count'];
 const daysElapsedKey = config['ccd']['daysElapsed'];
+const FETCH_RETRY_LIMIT = 3;
+const htmlElements = {
+    slider : document.getElementById("slider"),
+    sliderToolbar : document.getElementById("map-overlay-inner"),
+    maxCasesDiv :  document.getElementById("cases-max"),
+    dateLabel : document.getElementById("map-date")
+
+}
 
 // initializer functions -------------------------------------------------------
 // main initializer
-function mainInit() {
+async function mainInit() {
     drawBlankMap();
 
     // load erics
-    callService(config.geoJsonData_url, loadGeometryData);
+    let geoJsonPromise = fetch_retry(config.geoJsonData_url, FETCH_RETRY_LIMIT);
+    let countyCasesPromise = fetch_retry(config.countyCases_url, FETCH_RETRY_LIMIT);
 
-    // load ours
-    callService(config.countyCases_url, loadInitialData);
-
+    let [geoJson,countyCases] = await Promise.all([geoJsonPromise, countyCasesPromise]);
     displayFooterMessage("Loading initial county data, please wait...", false);
+    loadInitialData(geoJson, countyCases);
 }
 
 function drawBlankMap() {
@@ -44,66 +44,39 @@ function drawBlankMap() {
     });
 }
 
-// load erics while moving stuff around in the background
-function loadGeometryData(data) {
-    geoJson = JSON.parse(data);
-    displayFooterMessage("Map pre-load finished. Moving to background loading...", false);
-}
-
 // ajax call to populate county data with formated filter
-function loadInitialData(responseData) {
-    if (geoJson == null) // FIXME: See if there is a better way to do it
-    {
-        if (geoJsonDataAttempts < 2) {
-            console.log("GeoJson data is null. Will retry method in a moment...");
-            geoJsonDataAttempts++;
-            setTimeout(() => loadInitialData(responseData), 500)
-            
-        } else {
-            console.log("GeoJson data was too slow to load.");
-            displayFooterMessage("County level GeoJson data was empty. Try reloading the page in a minute or so.", true);
-        }
-    }
-    else {
-        if (responseData && responseData.length < 2) {
-            displayFooterMessage("Data was empty. We need to repopulate the database again...", true);
+function loadInitialData(geoJson, countyCases) {
+    let counties = countyCases.collection
+    let colorCodes = countyCases.colorCodes
+    lastDayElapsed = countyCases.lastAvailableDay;
+
+    for (var countyIndex = 0; countyIndex < counties.length; countyIndex++) {
+        var geoid = geoJson.features[countyIndex].properties.GEO_ID;
+        // just a double check if theres an inconsitency in data, we really just wanna break out of the loop
+        if (geoid != counties[countyIndex][config['ccd']['GEO_ID']]) {
+            displayFooterMessage("An error occured with combining erics data.", true);
+            console.log(geoid);
+            break;
         }
         else {
-            let countyCasesObject = JSON.parse(responseData);
-            let counties = countyCasesObject.collection
-            let colorCodes = countyCasesObject.colorCodes
-            lastDayElapsed = countyCasesObject.lastAvailableDay;
+            let cases = counties[countyIndex][config['ccd']['confirmed_cases']];
+            let deaths = counties[countyIndex][config['ccd']['deaths']];
+            addNiceDateToProp(cases);
+            addNiceDateToProp(deaths);
 
-            for (var countyIndex = 0; countyIndex < counties.length; countyIndex++) {
-                var geoid = geoJson.features[countyIndex].properties.GEO_ID;
-                // just a double check if theres an inconsitency in data, we really just wanna break out of the loop
-                if (geoid != counties[countyIndex][config['ccd']['GEO_ID']]) {
-                    displayFooterMessage("An error occured with combining erics data.", true);
-                    console.log(geoid);
-                    break;
-                }
-                else {
+            //For a property whose value decides the color codes
+            addColorCodes(geoJson, cases, colorCodes, lastDayElapsed);
 
-                    let cases = counties[countyIndex][config['ccd']['confirmed_cases']];
-                    let deaths = counties[countyIndex][config['ccd']['deaths']];
-                    addNiceDateToProp(cases);
-                    addNiceDateToProp(deaths);
-
-                    //For a property whose value decides the color codes
-                    addColorCodes(cases, colorCodes, lastDayElapsed);
-
-                    geoJson.features[countyIndex].properties['confirmed_cases'] = cases;
-                    geoJson.features[countyIndex].properties['deaths'] = deaths;
-                }
-            }
-            displayFooterMessage("Background loading complete. Map is fully ready.", false);
-
-            latestDateValue = addToDate(startDate, lastDayElapsed)
-            initMap(geoJson);
-            updateToolbarLimits();
+            geoJson.features[countyIndex].properties['confirmed_cases'] = cases;
+            geoJson.features[countyIndex].properties['deaths'] = deaths;
         }
     }
+    displayFooterMessage("Background loading complete. Map is fully ready.", false);
 
+    latestDateValue = addToDate(startDate, lastDayElapsed)
+    initMap(geoJson);
+    updateToolbarLimits();
+    
     //Local function 
     function addNiceDateToProp(prop) {
         if (prop.length > 0) {
@@ -117,44 +90,34 @@ function loadInitialData(responseData) {
     }
 
     //Local function
-    function addColorCodes(prop, colorCodes, lastDayElapsed) {
-        if (prop.length > 0) {
-            let colorCode; // current colorCode
+    function addColorCodes(geoJson, prop, colorCodes, lastDayElapsed) {
+        if (prop.length <= 0) {
+            return;
+        }
+        let colorCode;
+        // iterate from the first day in cases until today
+        for (let propIndex = 0; propIndex < prop.length; propIndex++) {
+            let daysElapsed = prop[propIndex][daysElapsedKey];
+            let caseCount = prop[propIndex][countKey];
+            maxPropValForColorCode = Math.max(caseCount,maxPropValForColorCode);
+            colorCode = getColorCode(colorCodes, caseCount);
 
-            // iterate from the first day in cases until today
-            for (let propIndex = 0; propIndex < prop.length; propIndex++) {
-                let daysElapsed = prop[propIndex][daysElapsedKey];
-                let caseCount = prop[propIndex][countKey];
-
-                // Get the max of all cases
-                if (caseCount > maxPropValForColorCode)
-                    maxPropValForColorCode = caseCount;
-
-                // Getting color code corresponding to caseCount from the function
-                colorCode = getColorCode(colorCodes, caseCount);
-
-                // next date where number of cases is changed
-                let nextAvailableDay = (propIndex != prop.length - 1) ? prop[propIndex + 1][daysElapsedKey]
-                    : lastDayElapsed;
-
-                // Add more color key value pairs with same color until next differential date is available
-                while (daysElapsed <= nextAvailableDay) {
-                    //Making colorcode as a property in the county object (required for mapbox)
-                    geoJson.features[countyIndex].properties[daysElapsed + "_color"] = colorCode;
-                    daysElapsed++;
-                }
+            // next date where number of cases is changed
+            let nextAvailableDay = (propIndex < prop.length-1) ? prop[propIndex + 1][daysElapsedKey] : lastDayElapsed;
+            while (daysElapsed <= nextAvailableDay) {
+                //Making colorcode as a property in the county object (required for mapbox)
+                geoJson.features[countyIndex].properties[daysElapsed + "_color"] = colorCode;
+                daysElapsed++;
             }
-            geoJson.features[countyIndex].properties['latestColor'] = colorCode;
         }
     }
 }
 
 function updateToolbarLimits() {
-    document.getElementById("cases-max").textContent = niceNumber(maxPropValForColorCode);
+    htmlElements.maxCasesDiv.textContent = niceNumber(maxPropValForColorCode);
     dateList = getDateArray(latestDateValue);
-    var slider = document.getElementById("slider");
-    slider.max = dateList.length - 1;
-    slider.value = date_diff_indays(startDate, latestDateValue);
+    htmlElements.slider.max = dateList.length - 1;
+    htmlElements.slider.value = date_diff_indays(startDate, latestDateValue);
 }
 
 
@@ -188,10 +151,7 @@ function initMap(geoJson) {
 
     // init of map with blank geojson
     map.on('load', function () {
-        map.addSource('county', {
-            'type': 'geojson',
-            'data': geoJson
-        });
+        map.addSource('county', {'type': 'geojson','data': geoJson});
         map.addLayer(countyLayerGeometry);
         map.on('mousemove', 'county-layer', function (e) {
             // Change the cursor style as a UI indicator.
@@ -205,12 +165,12 @@ function initMap(geoJson) {
         map.on('mouseenter', 'county', resetPopupProerties);
         
         // filter listener
-        document.getElementById("slider").addEventListener('input', e =>filterBy(e.target.value));
+        htmlElements.slider.addEventListener('input', e =>filterBy(e.target.value));
 
         // filter by initial slider position
         if (dateList) {
             filterBy(lastDayElapsed);
-            document.getElementById("map-overlay-inner").style.visibility = "visible";
+            htmlElements.sliderToolbar.style.visibility = "visible";
         }
     }); // eof map.onLoad
 
@@ -244,7 +204,6 @@ function getPopupContent(feature) {
             stringBuilder += "<br>No deaths in this county.";
         }
     }
-
     else {
         stringBuilder += "<br>There are no reported cases on this day.";
     }
@@ -277,24 +236,30 @@ function filterBy(date) {
     );
     currentDayValue = dateList[date];
     currentDate = niceDate(addToDate(startDate, dateList[date]));
-    document.getElementById("map-date").textContent = currentDate;
+    htmlElements.dateLabel.textContent = currentDate;
 }
 
 
 
 // function functions -------------------------------------------------------------
 
-function callService(url, callback) {
-    $.ajax({
-        method: "GET",
-        url: config.server_ip + url,
-        processData: false,
-        contentType: false,
-        encType: "multipart/form-data",
-        success: callback,
-        error: ajaxErrorHandle
-    });
-}
+async function fetch_retry(url,attempts){
+    try{
+        let response = await fetch(config.server_ip + url);
+        if(response.status!=200){
+            throw response.statusText;
+        }
+        return await response.json();
+        
+    }
+    catch(err){
+        if(attempts==0){
+            displayFooterMessage("An error occured while fetching data from the server.", true);
+            throw err;
+        }
+        return fetch_retry(url,attempts-1);
 
+    }
+}
 // call methods -------------------------------------------
-mainInit();
+mainInit(); 
