8dbdf3303fdf09605f08e1761f08228dd67f3022
Sai Shanmukha Narumanchi <nss10@outlook.com>
2021-02-13 11:11:44 -0600

    Added new collection called "metaCollection" and added two new methods

--- python/mdb.py
+++ python/mdb.py
@@ -11,16 +11,36 @@ def add_cases_data_to_collection(countyList):
 
 client = MongoClient(dbConf['uri'], dbConf['port'], username=dbConf['un'],password=dbConf['pwd'],authsource=dbConf['dbname'])
 db = client[dbConf["dbname"]]
-countyCollection = db[dbConf['countyLocationCollection']]
-
+countyCollectionName = dbConf['countyLocationCollection']
+countyCollection = db[countyCollectionName]
+metaCollection = db['metaDataCollection']
 
 def save_to_db(pvList,collectionName,userDefinedId=False):
-
     for pv in pvList:
         if(userDefinedId):
             pv['_id'] = str(pv['centerLat'])+str(pv['centerLon'])+pv['duration']['startTimestampMs']+pv['duration']['endTimestampMs']
         try:
-            collectionName.insert(pv)
+            collectionName.insert(pv, check_keys=False)
         except errors.DuplicateKeyError:
             pass
         del pv['_id']
+
+def add_new_records(geo_id, key_name, records):
+    countyCollection.update_one({"GEO_ID": geo_id},
+                                {"$addToSet": {
+                                        key_name: {
+                                            "$each": records
+                                        }
+                                    }
+    })
+
+
+def set_last_updated_date_in_db(key, dateString):
+    metaCollection.update_one({},{"$set":{key: dateString}},True)
+
+def get_last_updated_date_in_db(key):
+    return list(metaCollection.find({},{"_id":0}))[0][key]
+
+
+if __name__=="__main__":
+    set_last_updated_date_in_db("cases", "2021-02-01")
